97001b856b2d70809fc9130fd3eac2b5b6523eab
JM TV Mounting & Installation <jmch386@gmail.com>
2026-06-26 13:00:02 -0400

    Point hero logo to transparent asset

--- components/HeroBrandVideo.tsx
+++ components/HeroBrandVideo.tsx
@@ -5,10 +5,10 @@ export function HeroBrandVideo() {
   return (
     <div className="mx-auto mb-6 w-full max-w-[18rem] sm:max-w-[24rem] lg:max-w-[28rem] xl:max-w-[30rem]">
       <Image
-        src="/brand/hero-left-logo.jpg"
+        src="/brand/hero-left-logo-transparent.png"
         alt={brand.name}
-        width={900}
-        height={900}
+        width={1000}
+        height={1000}
         sizes="(max-width: 640px) 288px, (max-width: 1024px) 384px, 480px"
         className="h-auto w-full rounded-md object-contain"
         priority
