525109ebc152dbf753daf6960a2358e2f914b964
JM TV Mounting & Installation <jmch386@gmail.com>
2026-06-26 23:18:12 -0400

    Reduce hero logo size

--- components/HeroBrandVideo.tsx
+++ components/HeroBrandVideo.tsx
@@ -3,13 +3,13 @@ import { brand } from "@/lib/content";
 
 export function HeroBrandVideo() {
   return (
-    <div className="mx-auto mb-5 w-full max-w-[18rem] sm:max-w-[22rem] md:mx-0 lg:max-w-[26rem] xl:max-w-[28rem]">
+    <div className="mx-auto mb-6 w-full max-w-[15rem] sm:max-w-[18rem] md:mx-0 lg:max-w-[22rem] xl:max-w-[24rem]">
       <Image
         src="/brand/hero-left-logo-black.png"
         alt={brand.name}
         width={1126}
         height={989}
-        sizes="(max-width: 640px) 288px, (max-width: 1024px) 352px, 448px"
+        sizes="(max-width: 640px) 240px, (max-width: 1024px) 288px, 384px"
         className="h-auto w-full rounded-lg object-contain"
         priority
       />
