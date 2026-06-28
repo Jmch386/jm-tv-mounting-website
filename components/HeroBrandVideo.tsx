cd2add0593b6569735aaabb61693be884b5af8b8
JM TV Mounting & Installation <jmch386@gmail.com>
2026-06-28 07:45:11 -0400

    Replace hero logo with TV mount image

--- components/HeroBrandVideo.tsx
+++ components/HeroBrandVideo.tsx
@@ -5,10 +5,10 @@ export function HeroBrandVideo() {
   return (
     <div className="mx-auto mb-9 w-full max-w-[12.5rem] sm:max-w-[15rem] md:mx-0 lg:max-w-[18rem] xl:max-w-[20rem]">
       <Image
-        src="/brand/hero-left-logo-black.png"
-        alt={brand.name}
-        width={1126}
-        height={989}
+        src="/brand/hero-tv-mount-only.png"
+        alt={`${brand.name} TV mount icon`}
+        width={1254}
+        height={1254}
         sizes="(max-width: 640px) 200px, (max-width: 1024px) 240px, 320px"
         className="h-auto w-full rounded-lg object-contain"
         priority
